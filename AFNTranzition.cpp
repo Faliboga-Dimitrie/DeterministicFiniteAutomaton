bc7b974f4fb590aa6e65aba18a6b2711e31a57a4
RedMateria-hub <116025204+RedMateria-hub@users.noreply.github.com>
2024-11-28 00:47:28 +0200

    Delete AFNTranzition.cpp

--- AFNTranzition.cpp
+++
@@ -1,7 +0,0 @@
-#include "AFNTranzition.h"
-
-AFNTranzition::AFNTranzition(std::string fromState, std::string symbol, std::vector<std::string> toStates):
-	Tranzition(fromState, symbol),
-	m_toStates{ toStates }
-{
-}
