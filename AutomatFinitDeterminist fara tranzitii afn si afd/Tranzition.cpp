709236e8737838a3c40cbac75edb5781768a18f7
RedMateria-hub <116025204+RedMateria-hub@users.noreply.github.com>
2024-12-03 16:53:19 +0200

    Delete AutomatFinitDeterminist fara tranzitii afn si afd directory

--- AutomatFinitDeterminist fara tranzitii afn si afd/Tranzition.cpp
+++
@@ -1,8 +0,0 @@
-#include "Tranzition.h"
-
-Tranzition::Tranzition(std::string fromState, std::string symbol, std::string toState) :
-	m_fromState{ fromState },
-	m_symbol{ symbol },
-	m_toState{ toState }
-{
-}
