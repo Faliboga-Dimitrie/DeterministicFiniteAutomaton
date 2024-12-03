5a46f40637f6c28b9c836377f0376200e54304d5
RedMateria-hub <116025204+RedMateria-hub@users.noreply.github.com>
2024-12-03 16:51:43 +0200

    Delete AutomatFinitDeterminist/AFDTranzition.cpp

--- AutomatFinitDeterminist/AFDTranzition.cpp
+++
@@ -1,7 +0,0 @@
-#include "AFDTranzition.h"
-
-AFDTranzition::AFDTranzition(std::string fromState, std::string symbol, std::string toState) :
-	Tranzition(fromState, symbol),
-	m_toState{ toState }
-{
-}
